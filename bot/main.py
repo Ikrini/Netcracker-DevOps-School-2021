8a580782856f6026db7e7ffe652d3c3b6939ce19
Inom ikromov <ikrini@list.ru>
2021-06-07 05:51:57 +0300

    all-docker-update

--- bot/main.py
+++ bot/main.py
@@ -2,24 +2,12 @@
 # telegram bot
 from aiogram.utils import executor
 from bot import BotTelegram
-import os
-from flask import Flask
 
-PORT = int(os.environ.get('PORT', 8080))
 
 def main():
     bot_telegram = BotTelegram()
     executor.start_polling(bot_telegram.disp)
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
