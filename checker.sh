548ccccb573c898664f8956931155ae4e96a6f34
inom <inom.kolodin@mail.ru>
2021-07-09 19:59:14 +0000

    added sudo in checker

--- checker.sh
+++ checker.sh
@@ -7,10 +7,10 @@ echo ${WORKSPACE}
 if [ -f "$FILE" ]; then
     echo "secret file $FILE  exist."
     sudo rm $FILE
-    cp ${ConfigPy} /var/lib/jenkins/workspace/test_telebot
+    sudo cp ${ConfigPy} /var/lib/jenkins/workspace/test_telebot
 else
     echo "secret file $FILE does not exist."
     cat ${ConfigPy}
-    cp ${ConfigPy} /var/lib/jenkins/workspace/test_telebot
+    sudo cp ${ConfigPy} /var/lib/jenkins/workspace/test_telebot
     echo "secret file has been copied"
 fi
