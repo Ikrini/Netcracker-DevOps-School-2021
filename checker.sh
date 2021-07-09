3a66241b705dcd8b5fc9693fd00f9e90ff732bd2
inom <inom.kolodin@mail.ru>
2021-07-09 20:04:32 +0000

    remove sudo in checker & chmod workspace test_telebot

--- checker.sh
+++ checker.sh
@@ -6,11 +6,11 @@ echo ${WORKSPACE}
 
 if [ -f "$FILE" ]; then
     echo "secret file $FILE  exist."
-    sudo rm $FILE
-    sudo cp ${ConfigPy} /var/lib/jenkins/workspace/test_telebot
+    rm $FILE
+    cp ${ConfigPy} /var/lib/jenkins/workspace/test_telebot
 else
     echo "secret file $FILE does not exist."
     cat ${ConfigPy}
-    sudo cp ${ConfigPy} /var/lib/jenkins/workspace/test_telebot
+    cp ${ConfigPy} /var/lib/jenkins/workspace/test_telebot
     echo "secret file has been copied"
 fi
