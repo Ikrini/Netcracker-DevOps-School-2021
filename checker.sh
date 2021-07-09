aac109d453fe768ec68292b3d9ee1dfb17fba931
inom <inom.kolodin@mail.ru>
2021-07-09 19:52:55 +0000

    rewriten checker

--- checker.sh
+++ checker.sh
@@ -6,9 +6,11 @@ echo ${WORKSPACE}
 
 if [ -f "$FILE" ]; then
     echo "secret file $FILE  exist."
+    sudo rm $FILE
+    cp ${ConfigPy} /var/lib/jenkins/workspace/test_telebot
 else
     echo "secret file $FILE does not exist."
     cat ${ConfigPy}
-    cp ${env.ConfigPy} /var/lib/jenkins/workspace/test_telebot/
+    cp ${ConfigPy} /var/lib/jenkins/workspace/test_telebot
     echo "secret file has been copied"
 fi
