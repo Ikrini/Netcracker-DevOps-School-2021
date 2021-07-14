86e91a61af17a34f77629249511afadcb131ab38
inom <inom.kolodin@mail.ru>
2021-07-14 23:10:29 +0000

    fix version.sh

--- code/version.sh
+++ code/version.sh
@@ -7,10 +7,10 @@ echo ${BUILD_NUMBER}
 if [ -f "$VERSION" ]; then
     echo "file  $VERSION  exist."
     rm $VERSION
-    cat ${BUILD_NUMBER} >> $VERSION
+    echo ${BUILD_NUMBER} >> $VERSION
 else
     echo "file $VERSION does not exist."
-    cat ${BUILD_NUMBER} >> $VERSION
+    echo ${BUILD_NUMBER} >> $VERSION
     echo "version file has been copied"
 fi
 
